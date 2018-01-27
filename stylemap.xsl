693c63de37dd2d90b99f889653c7c1cfb8561ecd
nico <bobelet.nicolas@gmail.com>
2018-01-27 14:34:56 +0100

    TD fini

--- stylemap.xsl
+++ stylemap.xsl
@@ -8,15 +8,48 @@
 		indent="yes" ></xsl:output>
 
 		<xsl:template match="/carto">
-		
-
-			<xsl:apply-templates select="/carto/markers"/>
 			<div id="mapid"></div>
+<script>
 	
 
-		</xsl:template>
-		<xsl:template match="markers">
-		<xsl:value-of select ="marker/@name"/>
+
+	var mymap = L.map('mapid').setView([<xsl:value-of select="/carto/localisation/latitude"/>, 	<xsl:value-of select="/carto/localisation/longitude"/>], 15);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data ; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+	L.marker([	<xsl:value-of select="/carto/localisation/latitude"/>,	<xsl:value-of select="/carto/localisation/longitude"/>]).addTo(mymap)
+		.bindPopup("<b>Vous êtes Ici</b><br />").openPopup();
+
+
+	var popup = L.popup();
+	
+</script>
+
+			<xsl:apply-templates select="/carto/markers/marker"/>
+
 			
+	<xsl:apply-templates select="/carto/localisation"/>
 		</xsl:template>
+
+		<xsl:template match="marker">
+			
+			
+<script>
+	L.circleMarker([<xsl:value-of select="@lat"/>,<xsl:value-of select="@lng"/>]).addTo(mymap)
+		.bindPopup("<b> <xsl:value-of select="@address"/></b><br />").openPopup();
+</script>
+		</xsl:template>
+
+		<xsl:template match="localisation">
+			
+			<xsl:value-of select="latitude"/>
+			<xsl:value-of select="longitude"/>
+		</xsl:template>
+
 	</xsl:stylesheet>
--- tp.php
+++ tp.php
@@ -8,10 +8,10 @@ $xml = simplexml_load_string($str);
 // Récupère la lattitude et longitude
 $gps = ["$xml->lat","$xml->lon"];
 // Concatène la valeur
-$localisation = $gps[0].','.$gps[1];
+$localisation = array($gps[0],$gps[1]);
 
 // Stringify du XML avec la varaible localisation
-$data = file_get_contents('http://www.infoclimat.fr/public-api/gfs/xml?_ll='.$localisation.'&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2');
+$data = file_get_contents('http://www.infoclimat.fr/public-api/gfs/xml?_ll='.$localisation[0].','.$localisation[1].'&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2');
 
 $data = simplexml_load_string($data);
 
@@ -26,6 +26,9 @@ $proc->importStyleSheet($xsl); // attachement des règles xsl
 $data2 = file_get_contents('http://www.velostanlib.fr/service/carto');
 $data2 = simplexml_load_string($data2);
 
+$myloc=$data2->addChild('localisation');
+$myloc->addChild("latitude",$gps[0]);
+$myloc->addChild("longitude",$gps[1]);
 $xsl2 = new DOMDocument;
 $xsl2->load('stylemap.xsl');
 
@@ -43,58 +46,14 @@ $proc2->importStyleSheet($xsl2); // attachement des règles xsl
     <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
 
 
-</head>
 
+
+</head>
+<body>
 <?php
 echo $proc->transformToXML($data);
 echo $proc2->transformToXML($data2);
-echo ("<div class='hidden' id='lat'>".$gps[0]."</div>
-<div class='hidden' id='long'>".$gps[1]."</div>")
 ?>
-
-
-<script>
-	
-	let lat = document.querySelector("#lat").innerHTML;
-	let long = document.querySelector("#long").innerHTML;
-console.log(lat);
-	console.log(long);
-	var mymap = L.map('mapid').setView([lat, long], 13);
-
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data ; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
-
-	L.marker([48.9188, 2.254]).addTo(mymap)
-		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-	L.circle([48.9188, 2.254], 500, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-	}).addTo(mymap).bindPopup("I am a circle.");
-
-	L.polygon([
-		[51.509, -0.08],
-		[51.503, -0.06],
-		[51.51, -0.047]
-	]).addTo(mymap).bindPopup("I am a polygon.");
-
-
-	var popup = L.popup();
-
-	function onMapClick(e) {
-		popup
-			.setLatLng(e.latlng)
-			.setContent("You clicked the map at " + e.latlng.toString())
-			.openOn(mymap);
-	}
-
-	mymap.on('click', onMapClick);
-	
-</script>
+</body>
+</html>
 
